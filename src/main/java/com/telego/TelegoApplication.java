2654ca9c47bd4090636ee3f620feead09f8b690b
ahmaadaadel <ahmaadaadel@yahoo.co.uk>
2020-02-08 18:03:41 +0200

    Init spring boot project

---
+++ src/main/java/com/telego/TelegoApplication.java
@@ -0,0 +1,13 @@
+package com.telego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telego/TelegoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
