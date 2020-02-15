21f745f36c567b96fb5312c12255102f45f6e847
ahmaadaadel <ahmaadaadel@yahoo.co.uk>
2020-02-15 23:54:33 +0200

    adding lookupsAreas to AdminLookupsResponse

--- src/main/java/com/telego/model/response/AdminLookupsResponse.java
+++ src/main/java/com/telego/model/response/AdminLookupsResponse.java
@@ -2,6 +2,7 @@ package com.telego.model.response;
 
 import java.util.List;
 
+import com.telego.model.AreaDTO;
 import com.telego.model.CountryDTO;
 
 import lombok.AllArgsConstructor;
@@ -13,12 +14,13 @@ import com.telego.model.status.STATUS;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode(callSuper=true)
+@EqualsAndHashCode(callSuper = true)
 public class AdminLookupsResponse extends Response {
 
 	private STATUS status;
-	
+
 	private List<CountryDTO> lookupsCountries;
+	private List<AreaDTO> lookupsAreas;
 	// TODO lookupsAreas;
 	// TODO lookupsBoxes;
 	// TODO lookupsCabin;
