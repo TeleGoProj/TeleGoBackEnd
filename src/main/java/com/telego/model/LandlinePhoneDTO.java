ff0c40cce1c548a70c3ed7d789c9b68b8854463c
MohamedGew <mohamedgewaly@gmail.com>
2020-02-15 18:01:47 +0200

    commit done - Gew

---
+++ src/main/java/com/telego/model/LandlinePhoneDTO.java
@@ -0,0 +1,5 @@
+package com.telego.model;
+
+public class LandlinePhoneDTO {
+
+}
--- src/main/java/com/telego/model/request/AdminLookupsRequest.java
+++ src/main/java/com/telego/model/request/AdminLookupsRequest.java
@@ -3,7 +3,14 @@ package com.telego.model.request;
 import java.util.List;
 
 import com.telego.model.CountryDTO;
-
+import com.telego.model.AreaDTO;
+import com.telego.model.BoxDTO;
+import com.telego.model.CabinDTO;
+import com.telego.model.CityDTO;
+import com.telego.model.FeatureDTO;
+import com.telego.model.LandlinePhoneDTO;
+import com.telego.model.LogisticsDTO;
+import com.telego.model.PhoneUserDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -19,52 +26,86 @@ public class AdminLookupsRequest extends Request{
 	private List<CountryDTO> updatedCountries;
 	private List<CountryDTO> deletedCountries;
 	
-	// TODO addedAreas;
-	// TODO updatedAreas;
-	// TODO deletedAreas;
+	private List<AreaDTO> addedAreas;
+	private List<AreaDTO> updatedAreas;
+	private List<AreaDTO> deletedAreas;
+	
+	private List<BoxDTO> addedBoxes;
+	private List<BoxDTO> updatedBoxes;
+	private List<BoxDTO> deletedBoxes;
+	
+	private List<CabinDTO> addedCabins;
+	private List<CabinDTO> updatedCabins;
+	private List<CabinDTO> deletedCabins;
+	
+	private List<CityDTO> addedCities;
+	private List<CityDTO> updatedCities;
+	private List<CityDTO> deletedCities;
+	
+	private List<FeatureDTO> addedFeatures;
+	private List<FeatureDTO> updatedFeatures;
+	private List<FeatureDTO> deletedFeatures;
+	
+	private List<LandlinePhoneDTO> addedlandlinePhone;
+	private List<LandlinePhoneDTO> updatedlandPhone;
+	private List<LandlinePhoneDTO> deletedlandlinePhones;
+	
+	private List<LogisticsDTO> addedLogistics;
+	private List<LogisticsDTO> updatedLogistics;
+	private List<LogisticsDTO> deletedLogistics;
+	
+	private List<PhoneUserDTO> addedPhoneUsers;
+	private List<PhoneUserDTO> updatedPhoneUsers;
+	private List<PhoneUserDTO> deletedPhoneUsers;
 	
 	
-	// TODO addedBoxes;
-	// TODO updatedBoxes;
-	// TODO deletedBoxes;
+	// TODO addedAreas - Done;
+	// TODO updatedAreas - Done;
+	// TODO deletedAreas - Done;
 	
 	
-	// TODO addedCabin;
-	// TODO updatedCabin;
-	// TODO deletedCabin;
+	// TODO addedBoxes - Done;
+	// TODO updatedBoxes - Done;
+	// TODO deletedBoxes - Done;
 	
 	
-	// TODO addedCities;
-	// TODO updatedCities;
-	// TODO deletedCities;
+	// TODO addedCabin - Done;
+	// TODO updatedCabin - Done;
+	// TODO deletedCabin - Done;
+
 	
 	
-	// TODO addedCountries;
-	// TODO updatedCountries;
-	// TODO deletedCountries;
+	// TODO addedCities - Done;
+	// TODO updatedCities - Done;
+	// TODO deletedCities - Done;
 	
 	
-	// TODO addedFeatures;
-	// TODO updatedFeatures;
-	// TODO deletedFeatures;
+	// TODO addedCountries - already Done;
+	// TODO updatedCountries - already Done;
+	// TODO deletedCountries - already Done;
 	
 	
-	// TODO addedLinePhones;
-	// TODO updatedPhones;
-	// TODO deletedLinePhones;
+	// TODO addedFeatures - Done;
+	// TODO updatedFeatures - Done;
+	// TODO deletedFeatures - Done;
 	
 	
-	// TODO addedLogistics;
-	// TODO updatedLogistics;
-	// TODO deletedLogistics;
+	// TODO addedLinePhones - Done;
+	// TODO updatedPhones - Done;
+	// TODO deletedLinePhones - Done;
+	
+
+	// TODO addedLogistic - Done;
+	// TODO updatedLogistics - Done;
+	// TODO deletedLogistics - Done;
 	
 	
-	// TODO addedFeatures;
-	// TODO updatedFeatures;
-	// TODO deletedFeatures;
+	// TODO addedFeatures - Duplicated;
+	// TODO updatedFeatures - Duplicated;
+	// TODO deletedFeatures - Duplicated;
 	
 	
-	// TODO addedPhoneUsers;
-	// TODO updatedPhoneUsers;
-	// TODO deletedPhoneUsers;
+	// TODO addedPhoneUsers - Done;
+	// TODO updatedPhoneUsers - Done;
+	// TODO deletedPhoneUsers - Done;
 }
