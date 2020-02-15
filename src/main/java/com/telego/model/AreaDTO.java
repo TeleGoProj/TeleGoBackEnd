ab099c8aeeaf09b100697633d6f94e2bba8e6986
ahmaadaadel <ahmaadaadel@yahoo.co.uk>
2020-02-15 18:08:30 +0200

    Adding attributes of AreaDTO

--- src/main/java/com/telego/model/AreaDTO.java
+++ src/main/java/com/telego/model/AreaDTO.java
@@ -1,5 +1,21 @@
 package com.telego.model;
 
-public class AreaDTO {
+import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class AreaDTO {
+	private Long areaId;
+	private String areaNameEn;
+	private String areaNameAr;
+	private CityDTO cityByCityId;
+	private List<BoxDTO> boxesByAreaId;
+	private List<LogisticsDTO> logisticsByAreaId;
 }
